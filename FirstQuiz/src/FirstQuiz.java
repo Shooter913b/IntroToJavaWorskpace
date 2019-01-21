0b0533ba29502b1e5457fee0b81f0d1f115a58d1
Infant Derrick Gnanasusairaj <infant.derrick.gnanasusairaj@gmail.com>
2019-01-21 16:49:18 -0500

    stubs generated

---
+++ FirstQuiz/src/FirstQuiz.java
@@ -0,0 +1,18 @@
+public class FirstQuiz {
+    //TODO 4-create test code to demonstrate your understanding of not crashing the program for all cases
+    public static void main(String[] args){
+
+    }
+    //TODO 1-given a year, month and day, return a string that reads "Today is 'month' 'day', 'year'"
+    public static String formatDate(int year, String month, int day){
+
+    }
+    //TODO 2-given a double fahrenheit return the appropriate celsius
+    public static double convertFtoC(double f){
+
+    }
+    //TODO 3-given a time in seconds, print it into days, hours, mins, seconds and if its singular it should be singular and if its 0 its should not be displayed
+    public static void convertTime(int seos){
+
+    }
+}
