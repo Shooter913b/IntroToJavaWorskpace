d8ff4c01c985266783fc12973704215c4cf14664
Infant Derrick Gnanasusairaj <infant.derrick.gnanasusairaj@gmail.com>
2019-01-21 16:54:33 -0500

    HelloRick.java has been generated

---
+++ FirstQuiz/src/HelloRicky.java
@@ -0,0 +1,7 @@
+public class HelloRicky {
+    //TODO any testing you want
+    public static void main(String args[]){
+
+    }
+    //TODO Make a scanner and take in a name and then print out the name and give them a greating
+}
